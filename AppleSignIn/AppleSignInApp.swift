4160ba20ed2beeec221d5ecca28995072b467082
OsmanM94 <osmanmunur@yahoo.com>
2024-05-22 11:56:10 +0100

    Initial Commit

---
+++ AppleSignIn/AppleSignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSignInApp.swift
+//  AppleSignIn
+//
+//  Created by asia on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleSignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
